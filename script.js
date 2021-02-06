d39444526537fb39eaf58e3856aff18ab3c87c5a
Kat Huang <khuang129@gmail.com>
2021-02-05 16:32:50 -0800

    dog gallery, keydown, fixed button and nav issues in carousel

--- script.js
+++ script.js
@@ -4,37 +4,6 @@ const carouselButtons = document.querySelectorAll('.carousel-button');
 
 const numOfImages = document.querySelectorAll('.carousel-images img').length;
 
-let imageIndex = 1;
-
-let translateX = 0;
-
-//event listener for clicking carousel buttons
-
-carouselButtons.forEach(button => {
-    button.addEventListener('click', event => {
-        event.stopPropagation();
-         if (event.target.id === 'previous') {
-             if(imageIndex !== 1) {
-                 imageIndex--;
-                 translateX += 480;
-             } else {
-                imageIndex = numOfImages;
-                translateX = 480 - (480 * numOfImages);
-             }
-         } else {
-             if(imageIndex !== numOfImages) {
-                imageIndex++;
-                translateX -= 480;
-             } else {
-                 imageIndex = 1;
-                 translateX = 0;
-             }
-         }
-
-        carouselImages.style.transform = `translateX(${translateX}px)`;
-    });
-})
-
 //click on image in grid gallery, have carousel modal pop up.
 
 const squares = document.querySelectorAll('.grid-container img');
@@ -45,25 +14,61 @@ squares.forEach(square => {
 
 const carouselModal = document.getElementById('carouselModal');
 
-console.log(squares);
 
 function openCarousel(e) {
-    console.log(e.target);
-    
-    const number = Number(e.target.id.split('-')[1])
+    const number = Array.prototype.findIndex.call(squares, (square) => square === e.target);
 
-    const translateX = `-${(480*(number)-480)}px`;
+    let translateX = `-${(480*number)}px`;
+    let clickAxis = -480 * number;
 
     carouselImages.style.transform = `translateX(${translateX})`
     carouselModal.classList.add("carousel-modal--open");
+
+    //event listener for clicking carousel buttons
+    carouselButtons.forEach(button => {
+        button.addEventListener('click', event => {
+            if (event.target.id === 'previous') {
+                if(clickAxis !== 0) {
+                    clickAxis += 480;
+                } else {
+                    clickAxis = 480 - (480 * numOfImages);
+                }
+            } else {
+                if(clickAxis !== (-480 * (numOfImages - 1))) {
+                    clickAxis -= 480;
+                } else {
+                    clickAxis = 0;
+                }
+            }
+            carouselImages.style.transform = `translateX(${clickAxis}px)`;
+            event.stopPropagation();
+        });
+    })
+
+    if (carouselModal.classList.contains('carousel-modal--open')) {
+        document.addEventListener('keydown', e => {
+            if (e.key === 'ArrowLeft') {
+                if(clickAxis !== 0) {
+                    clickAxis += 480;
+                } else {
+                    clickAxis = 480 - (480 * numOfImages);
+                }
+            } 
+            if (e.key === 'ArrowRight') {
+                if(clickAxis !== (-480 * (numOfImages - 1))) {
+                    clickAxis -= 480;
+                } else {
+                    clickAxis = 0;
+                }
+            }
+            carouselImages.style.transform = `translateX(${clickAxis}px)`; 
+        })
+    }
+
 }
 
+//close carousel modal when clicking on modal
 carouselModal.addEventListener('click', closeCarousel);
-
 function closeCarousel() {
     carouselModal.classList.remove('carousel-modal--open');
 }
-
-//when clicking on a square, open carousel modal to that matching image
-    //if i click on square-1, open imageindex1, 
-
